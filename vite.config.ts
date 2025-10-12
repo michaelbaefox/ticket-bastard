6705882b85ce3ea33a80b685b8956d1067a6610d
michaelbaefox <michaelbaefox@icloud.com>
2025-10-12 09:10:31 -0700

    Update package dependencies and add deployment scripts
    - Added `gh-pages` dependency for deployment purposes.
    - Updated `package.json` with new `predeploy` and `deploy` scripts for streamlined deployment to GitHub Pages.
    - Enhanced `package-lock.json` with new dependencies and their respective versions for better package management.
    - Modified `vite.config.ts` to set a dynamic base path based on the environment mode.

--- vite.config.ts
+++ vite.config.ts
@@ -3,17 +3,23 @@ import react from "@vitejs/plugin-react-swc";
 import path from "path";
 
 // https://vitejs.dev/config/
-export default defineConfig(({ mode }) => ({
-  server: {
-    host: "::",
-    port: 8080,
-  },
-  plugins: [
-    react(),
-  ],
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src"),
+export default defineConfig(({ mode }) => {
+  const isDevelopmentMode = mode === "development";
+  const basePath = isDevelopmentMode ? "/" : "/ticket-bastard/";
+
+  return {
+    base: basePath,
+    server: {
+      host: "::",
+      port: 8080,
     },
-  },
-}));
+    plugins: [
+      react(),
+    ],
+    resolve: {
+      alias: {
+        "@": path.resolve(__dirname, "./src"),
+      },
+    },
+  };
+});
